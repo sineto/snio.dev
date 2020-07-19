02a19f07d28e57b4da4a7f0883dd3c6051b2df09
Sinésio Neto <sn.logbackup@gmail.com>
2020-07-19 14:35:02 -0300

    bug: not apply css

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
     content: ["./components/**/*.js", "./pages/**/*.js"],
+		whitelist:['body', 'html'],
     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
   }
 ];
