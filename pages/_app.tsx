e8a15669d1e87a85371084825e4415f02fd665e1
Sinésio Neto <sn.sineto@gmail.com>
2021-01-25 21:48:26 -0300

    chore: add Helmet to apply Roboto font from google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import { Helmet } from 'react-helmet';
 import { ThemeProvider } from 'styled-components';
 
 import theme from '../styles/theme';
@@ -7,6 +8,10 @@ import GlobalStyle from '../styles/global';
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href='https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400;500;600;700&family=Roboto:wght@100;300;400;500;700;900&display=swap' rel='stylesheet' />
+      </Helmet>
       <Component {...pageProps} />
       <GlobalStyle />
     </ThemeProvider>
