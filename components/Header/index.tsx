896522b29dc082a2c857ca06514331f66cf1ffb5
Sinésio Neto <sn.sineto@gmail.com>
2021-01-25 23:37:47 -0300

    refactor: componentize header section in pieces of components

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image';
-
 import TopBar from './TopBar';
 import HeroContent from './HeroContent';
 import * as Styles from './styles';
 
 const Header = (): JSX.Element => {
-
   return (
     <Styles.Header>
       <TopBar />
