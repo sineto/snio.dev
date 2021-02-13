0090ad5e490c2dd4c0e1e2ffd20d799929341c8e
Sinésio Neto <sn.sineto@gmail.com>
2021-02-13 15:32:22 -0300

    ci: fix typo

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_FEF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_FEF" == "develop" || "$VERCEL_GIT_COMMIT_FEF" == "master" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "master" ]]; then
   echo "==> Build can proceed"
   exit 1;
 else
