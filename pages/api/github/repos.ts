0396f38bf26e7f5deb98b5771f9ff1e181e6e234
Sinésio Neto <sn.sineto@gmail.com>
2021-02-14 19:47:28 -0300

    feat: fetch github api data and filter defined users repositories

--- pages/api/github/repos.ts
+++ pages/api/github/repos.ts
@@ -1,11 +1,22 @@
+import axios, { AxiosPromise } from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const repos = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
+const users: string[] = ['sineto', 'sinetoami'];
+
+const filterRepositories = async (users: string[]): Promise<AxiosPromise[]> => {
+  let repositories: AxiosPromise[];
+  for await (const user of users) {
+    const { data } = await axios.get(`https://api.github.com/users/${user}/repos`);
+    repositories = [...data];
+  }
+
+  return repositories;
+};
+
+const repos = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   try {
-    res.status(200).json({ title: 'test github' });
+    const repositories = await filterRepositories(users);
+    res.status(200).json({ repositories });
   } catch (error) {
     res.status(500).send(error);
   }
