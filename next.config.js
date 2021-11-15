aa20410137dfbe52e0d6c734ff64aef34c47b31e
Sinésio Neto <sn.sineto@gmail.com>
2021-11-15 14:41:43 -0300

    fix: cors api

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://snio.dev' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
