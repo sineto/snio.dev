fed1cd5ae8af8bfece5235a25b03fa0533aae28a
Sinesio Neto <git@sn1o.dev>
2026-04-20 02:34:19 -0300

    chore(site): update dependency packages and add Astro integration

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations:[icon()],
   fonts: [{
     provider: fontProviders.fontsource(),
     name: "Roboto Mono",
-    cssVariable: "--font-roboto-mono"
+    cssVariable: "--font-roboto-mono",
+    weights: [100, 200, 300, 400, 500, 600, 700]
   }],
 });
